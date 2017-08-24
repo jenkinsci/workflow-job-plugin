0f455a90035e1e385eaa0f2ab42d94792ac4dc07
Jesse Glick <jglick@cloudbees.com>
2017-08-24 17:21:31 -0400

    [JENKINS-29537] Take advantage of Run.getEnvironment calling ParametersAction.buildEnvironment.

--- src/main/java/org/jenkinsci/plugins/workflow/job/WorkflowRun.java
+++ src/main/java/org/jenkinsci/plugins/workflow/job/WorkflowRun.java
@@ -44,8 +44,6 @@ import hudson.console.LineTransformationOutputStream;
 import hudson.console.ModelHyperlinkNote;
 import hudson.model.Executor;
 import hudson.model.Item;
-import hudson.model.ParameterValue;
-import hudson.model.ParametersAction;
 import hudson.model.Queue;
 import hudson.model.Result;
 import hudson.model.Run;
@@ -431,13 +429,6 @@ public final class WorkflowRun extends Run<WorkflowJob,WorkflowRun> implements F
             }
         }
 
-        // TODO EnvironmentContributingAction does not support Job yet:
-        ParametersAction a = getAction(ParametersAction.class);
-        if (a != null) {
-            for (ParameterValue v : a) {
-                v.buildEnvironment(this, env);
-            }
-        }
         EnvVars.resolve(env);
         return env;
     }
