c33437fa9715d371b5a12c699c25dc6794e813f4
Jesse Glick <jglick@cloudbees.com>
2024-11-07 15:35:21 -0500

    Various test failures; this change is not intended for running builds

--- src/main/java/org/jenkinsci/plugins/workflow/job/WorkflowRun.java
+++ src/main/java/org/jenkinsci/plugins/workflow/job/WorkflowRun.java
@@ -553,9 +553,11 @@ public final class WorkflowRun extends Run<WorkflowJob,WorkflowRun> implements F
 
         // super.reload() forces result to be FAILURE, so working around that
         new XmlFile(XSTREAM,new File(getRootDir(),"build.xml")).unmarshal(this);
-        var _execution = execution;
-        if (_execution != null) {
-            _execution.onLoad(new Owner(this));
+        if (Boolean.TRUE.equals(this.completed)) {
+            var _execution = execution;
+            if (_execution != null) {
+                _execution.onLoad(new Owner(this));
+            }
         }
     }
 
