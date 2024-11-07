8e33232b5dcee7ecf8a4935514da6b09fd1ba03f
Jesse Glick <jglick@cloudbees.com>
2024-11-07 16:44:01 -0500

    SpotBugs possibly being too cautious

--- src/main/java/org/jenkinsci/plugins/workflow/job/WorkflowRun.java
+++ src/main/java/org/jenkinsci/plugins/workflow/job/WorkflowRun.java
@@ -554,8 +554,11 @@ public final class WorkflowRun extends Run<WorkflowJob,WorkflowRun> implements F
         // super.reload() forces result to be FAILURE, so working around that
         new XmlFile(XSTREAM,new File(getRootDir(),"build.xml")).unmarshal(this);
         synchronized (getMetadataGuard()) {
-            if (Boolean.TRUE.equals(completed) && execution != null && executionLoaded) {
-                execution.onLoad(new Owner(this));
+            if (Boolean.TRUE.equals(completed) && executionLoaded) {
+                var _execution = execution;
+                if (_execution != null) {
+                    _execution.onLoad(new Owner(this));
+                }
             }
         }
     }
